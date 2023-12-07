c98da75c29ce51118026dea12722e09007659fe9
jkeister0803 <117704060+jkeister0803@users.noreply.github.com>
2023-12-06 19:07:42 -0500

    Added second test

--- tests/todoList.spec.ts
+++ tests/todoList.spec.ts
@@ -6,3 +6,13 @@ test('can create a new todo list', async ({ page }) => {
     await page.keyboard.press('Enter');
     await expect(page.getByText('Tests')).toBeVisible();
 });
+
+test('can access todos list', async ({ page }) => {
+    await page.goto('https://eviltester.github.io/simpletodolist/todolists.html');
+    await page.getByPlaceholder('Enter new todo list name here').fill('Tests');
+    await page.keyboard.press('Enter');
+    await expect(page.getByText('Tests')).toBeVisible();
+    await page.locator('div').filter({ hasText: '[use] Tests' }).getByRole('link').click();
+    await expect(page).toHaveURL('https://eviltester.github.io/simpletodolist/todo.html#/&Tests');
+    await expect(page.getByRole('heading', { name: 'TODOs : Tests'})).toBeVisible();
+});
