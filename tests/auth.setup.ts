3596d119165a91345cfa10e267e509e3a3c1a925
jkeister0803 <117704060+jkeister0803@users.noreply.github.com>
2023-12-11 18:22:19 -0500

    added test data file for centralized management

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,11 +1,8 @@
-import { test as setup, expect } from '@playwright/test';
+import { test as setup } from '@playwright/test';
 import { HomePage } from '../pages/home-page';
 import { LoginPage } from '../pages/login-page';
 
-const URL = 'https://eviltester.github.io/simpletodolist/todolists.html';
-const adminURL = 'https://eviltester.github.io/simpletodolist/adminview.html';
-const authFile = 'playwright/.auth/user.json';
-const loginLinkName = 'Admin Login';
+const testData = JSON.parse(JSON.stringify(require('../testData.json')));
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 let homePage: HomePage;
@@ -14,9 +11,9 @@ let loginPage: LoginPage;
 setup('authenticate', async ({ page }) => {
     homePage = new HomePage(page);
     loginPage = new LoginPage(page);
-    await page.goto(URL);
-    await homePage.goToPage(loginLinkName);
+    await page.goto(testData.URL);
+    await homePage.goToPage(testData.loginLinkName);
     await loginPage.login(username, password);
-    await page.waitForURL(adminURL);
-    await page.context().storageState({ path: authFile });
+    await page.waitForURL(testData.adminURL);
+    await page.context().storageState({ path: testData.authFile });
 });
